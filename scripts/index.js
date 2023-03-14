5d40d10acc6287fc5e85cf1806ddcf83476bfa51
l1qwy <liqwy@yandex.ru>
2023-03-14 03:05:47 +0300

    popup__close

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,28 @@ const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 
 //добавляем фунцию для отображения модального окна
+//находим форму и поля в модалке
+let formElement = popupElement.querySelector('.form');
+let fieldName = formElement.querySelector('.form__field_name');
+let fieldJob = formElement.querySelector('.form__field_job');
+
+//находми элементы профиля
+let nameDefault = document.getElementById('name');
+let jobDefault = document.getElementById('job');
+
 const openPopup = function () {
   popupElement.classList.add("popup_open");
+  //записываем в поля формы данные значения свойства порфиля
+  fieldName.value = nameDefault.textContent;
+  fieldJob.value = jobDefault.textContent;
 };
 
+//фунция закрытия по клику на close
 const closePopup = function () {
   popupElement.classList.remove("popup_open");
 };
 
+// фунция закрытия по клику за пределами модалки
 const closePopupByClickOnOverlay = function(event) {
   if (event.target !== event.currentTarget) {
     return; 
@@ -20,65 +34,41 @@ const closePopupByClickOnOverlay = function(event) {
   closePopup();
 };
 
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(event) {
+    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    let nameValue = fieldName.value;
+    let jobValue = fieldJob.value;
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+    let nameOutput = document.querySelector('.profile__name');
+    let jobOutput = document.querySelector('.profile__description');
+
+    // Вставьте новые значения с помощью textContent
+    nameOutput.textContent = nameValue;
+    jobOutput.textContent = jobValue;
+
+    closePopup();
+};
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
 //обработчики кликов
 popupOpenButtonElement.addEventListener("click", openPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
 
 
-//Отправка формы
-// Находим форму в DOM
-let formElement = popupElement.querySelector('.form');
-console.log(formElement)
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('name');
-let jobInput = formElement.querySelector('job');
-
-console.log(nameInput);
-console.log(jobInput);
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit(event) {
-    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let nameValue = document.getElementsByTagName('input')[0].value;
-    let jobValue = document.getElementsByTagName('input')[1].value;
-
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let nameOutput = document.querySelector('.profile__name');
-    console.log(nameOutput.textContent); 
-
-    let jobOutput = document.querySelector('.profile__description');
-    console.log(jobOutput.textContent); 
-    
-    
-    // Вставьте новые значения с помощью textContent
-    nameOutput.textContent = nameValue;
-    console.log(nameOutput.textContent);
-
-    jobOutput.textContent = jobValue;
-    console.log(jobOutput.textContent); 
-
-    closePopup();
-
-};
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
-
-
 // реализация сердечка, испытание toggle
-const emotionElements = document.querySelector('.galery__item-emotion');
-console.log(emotionElements);
+//const emotionElements = document.querySelector('.galery__item-emotion');
 
-const toggleEmotion = function () {
-  emotionElements.classList.toggle('galery__item-emotion_active');
-};
+//const toggleEmotion = function () {
+//  emotionElements.classList.toggle('galery__item-emotion_active');
+//};
 
-emotionElements.addEventListener('click', toggleEmotion);
+//emotionElements.addEventListener('click', toggleEmotion);
 
